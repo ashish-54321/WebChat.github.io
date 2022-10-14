9c6fbf95b3e6e6ef0b896cf75999214b884bab1f
Ashish <115717632+ashish-54321@users.noreply.github.com>
2022-10-15 00:14:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
 //node server which will handells socket.io conections
 
+
+
+
+
+
+
+
+
 const express = require('express')
 const app = express()
 
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT||5000 
+const PORT = process.env.PORT || 5000
 
 
 http.listen(PORT, () => {
     console.log (`Listening on port ${PORT}`) 
 })
-// app.use(express.static(__dirname + '/public')) 
+app.use(express.static(__dirname + '/public')) 
 
 app.get('/', (req,res) =>{
-   res.sendFile(__dirname + '/chat.html')
-    // app.use(express.static(__dirname + 'chat.css')) 
+   
+    res.sendFile(__dirname + '/chat.html')
+    
     
 } )
-
+ 
 /// [ socket.io]/// 
 const io = require('socket.io')(http)
-
 var users = {};
+
 
 
 io.on('connection', (socket) =>{
@@ -36,8 +45,10 @@ io.on('connection', (socket) =>{
 
         
     })
+      
 
     socket.on('message' , (msg) =>{
         socket.broadcast.emit('message', msg)
+
     })
 })
