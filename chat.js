361810b398082a76f896cc14af31377f886403c1
Ashish <115717632+ashish-54321@users.noreply.github.com>
2022-10-15 18:49:32 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,26 +3,22 @@ console.log('this is chat js ')
 
 var names;
 var dropdown = document.querySelector(".dropdown-menu")
+var container =document.querySelector(".msgbox")
 
 function closeForm() {
       
     document.getElementById("myForm").style.display = "none";
     names = document.getElementById("email").value;
 
-    
     if (!names) {
         window.location.reload(); 
     }
         
-        
-    
-
     document.getElementById("logicchat").style.display = "block";
 
     socket.emit('joined-user', names);
 
 }
-
 
 
 socket.on('user-connected', (socket_name) => {
@@ -47,54 +43,25 @@ function userJoinLeft(name, status) {
 }
 
 
-
-
-
-
-
-
-
-// do {
-//     names = prompt("Enter your NAME TO JOIN");
-
-// } while (!names);
-
-
-
-
-
-
-
 ///////////////////////////////////////// [ send Message ]/////////////////////////////////////////////////
-
 
 
 function text() {
 
     var x = document.getElementById("exampleFormControlTextarea1").value;
-    // 
-    //var nextPage= document.getElementById("nexthtml");
+   
     var div = document.createElement("div");
-    div.style.width = "300px";
-    div.style.padding = "10px";
-    // div.style.margin = "15%";
-    div.style.marginTop = "1%";
-    div.style.marginBottom = "1%";
-    div.style.marginRight = "26%";
-    div.style.borderRadius = "16px";
-    // div.style.border = "15px solid green";
-    // div.style.marginLeft= "80%";
-    div.style.background = "#ffffff";
+   
     div.innerHTML = x;
-    document.getElementById("get").appendChild(div);
-    // document.getElementById("nexthtml").appendChild(div );
+    div.classList.add("message");
+    div.classList.add("income");
+    container.appendChild(div);
+  
 
     document.getElementById("exampleFormControlTextarea1").value = "";
-    // document.body.scrollHeight = 0;
-    // document.documentElement.scrollTop = 0;
+  
     window.scrollTo(0, document.body.scrollHeight);
-    /////////////////////////////////////////////////////////////////////
-
+    
     let msg = {
         user: names,
         message: x
@@ -109,41 +76,18 @@ function text() {
 
 socket.on('message', (msg) => {
 
-
-
-
-
-    // <h4>${msg.user}</h4>
     let markup = `
     
     <p>${msg.message}</p>
     `
-
-
-
     var div = document.createElement("div");
-    div.style.width = "300px";
-    div.style.padding = "10px";
-    // div.style.margin = "15%";
-    // div.style.border = "15px solid green";
-    div.style.marginLeft = "15%";
-    div.style.marginTop = "1%";
-    div.style.marginBottom = "1%";
-    div.style.background = "#DCF8C6";
-    div.style.borderRadius = "16px";
-
+    
     div.innerHTML = markup;
-    document.getElementById("get").appendChild(div);
+    div.classList.add("message");
+    div.classList.add("outcome");
+    container.appendChild(div);
 
     window.scrollTo(0, document.body.scrollHeight);
-
-
-    /////////////////////////[online ]///////////////////////
-
-
-
-
-
 })
 
 
@@ -152,12 +96,9 @@ socket.on('message', (msg) => {
 function clearchat() {
 
     var x = document.getElementById("loader");
-    // if (x.style.display === "none") {
+   
     x.style.display = "block";
-    // } else {
-    //   x.style.display = "none";
-    // }
-
+   
     window.scrollTo(0, document.body.scrollHeight);
     window.scrollTo(0, document.body.scrollHeight);
     window.scrollTo(0, document.body.scrollHeight);
