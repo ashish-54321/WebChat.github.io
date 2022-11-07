674ff606f1779b94897abe8e06aa9f589c26b8a8
Ashish <115717632+ashish-54321@users.noreply.github.com>
2022-11-07 21:55:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,14 +56,4 @@ io.on('connection', (socket) => {
 
     })
     
-     socket.on("upload", (file, callback) => {
-        console.log(file); // <Buffer 25 50 44 ...>
-
-        // save the content to the disk, for example
-        fs.writeFile("upload.jpg", file, (err) => {
-            callback({ message: err ? "failure" : "success" });
-        });
-    });
-
-
 })
